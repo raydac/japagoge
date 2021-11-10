31ecfb099fd0950e47cfb91a7024d6e270276683
Igor Maznitsa <rrg4400@gmail.com>
2021-11-10 10:25:57 +0200

    added support of 'japagoge.force.robot' boolean system property

--- japagoge-app/src/main/java/com/igormaznitsa/japagoge/grabbers/ScreenAreaGrabberFactory.java
+++ japagoge-app/src/main/java/com/igormaznitsa/japagoge/grabbers/ScreenAreaGrabberFactory.java
@@ -11,6 +11,8 @@ public final class ScreenAreaGrabberFactory {
 
   private static final Logger LOGGER = Logger.getLogger(ScreenAreaGrabberFactory.class.getName());
 
+  private static final String PROPERTY_FORCE_ROBOT = "japagoge.force.robot";
+
   private ScreenAreaGrabberFactory() {
 
   }
@@ -30,9 +32,13 @@ public final class ScreenAreaGrabberFactory {
 
   public ScreenAreaGrabber makeGrabber(final GraphicsDevice device) {
     ScreenAreaGrabber result = null;
-    if (SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_SOLARIS) {
-      LOGGER.info("Detected potential X11 system");
-      result = tryMakeGrabber("com.igormaznitsa.japagoge.grabbers.X11ScreenAreaGrabber", device);
+    if (Boolean.getBoolean(PROPERTY_FORCE_ROBOT)) {
+      LOGGER.info("Detected " + PROPERTY_FORCE_ROBOT + " as TRUE");
+    } else {
+      if (SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_SOLARIS) {
+        LOGGER.info("Detected potential X11 system");
+        result = tryMakeGrabber("com.igormaznitsa.japagoge.grabbers.X11ScreenAreaGrabber", device);
+      }
     }
     return result == null ? new RobotScreenAreaGrabber(device) : result;
   }
